247be0a7f16c93b462f1f7ad2baf1165bb3943b7
viltersander <sander.vilter@ametikool.ee>
2023-05-22 22:06:56 +0300

    initial commit

--- src/modules/common/components/maps/ContactsPage.tsx
+++ src/modules/common/components/maps/ContactsPage.tsx
@@ -19,34 +19,34 @@
 //   ))
 // );
 
-// const ContactsPage: React.FC = () => {
+const ContactsPage: React.FC = () => {
 //   const googleMapsApiKey = ''; // Replace with your actual API key
 
-//   return (
-//     <div className="p-6">
-//     <div id="contact" style={{ position: 'relative', height: '400px' }}>
-//       <MapComponent
-//         googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}`}
-//         loadingElement={<div className="h-full" />}
-//         containerElement={<div className="h-full" />}
-//         mapElement={<div className="h-full" />}
-//       />
-//       <div className="absolute top-6 sm:top-10 md:top-10 lg:top-10 left-8 bg-white p-4 shadow-xl border-2 border-black rounded-lg">
-//         <h2>Böns Pagaristuudio</h2>
-//         <p>Kohtu 1a, Kuressaare, Estonia</p>
-//         <p>Tel 5335 8141</p>
-//         <p>bonspagar@gmail.com</p>
-//       </div>
-//       <div className="absolute top-64 sm:top-16 md:top-16 lg:top-16 right-16 sm:right-12 md:right-12 lg:right-12
-//        bg-white p-4 shadow-xl border-2 border-black rounded-lg">
-//         <h2>Avatud</h2>
-//         <p>T - P kell 9.00 - 16.00</p>
-//         <p>E - SULETUD</p>
-//       </div>
+  return (
+    <div className="p-6">
+    <div id="contact" style={{ position: 'relative', height: '400px' }}>
+      {/* <MapComponent
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}`}
+        loadingElement={<div className="h-full" />}
+        containerElement={<div className="h-full" />}
+        mapElement={<div className="h-full" />}
+      /> */}
+      <div className="absolute top-6 sm:top-10 md:top-10 lg:top-10 left-8 bg-white p-4 shadow-xl border-2 border-black rounded-lg">
+        <h2>Böns Pagaristuudio</h2>
+        <p>Kohtu 1a, Kuressaare, Estonia</p>
+        <p>Tel 5335 8141</p>
+        <p>bonspagar@gmail.com</p>
+      </div>
+      <div className="absolute top-64 sm:top-16 md:top-16 lg:top-16 right-16 sm:right-12 md:right-12 lg:right-12
+       bg-white p-4 shadow-xl border-2 border-black rounded-lg">
+        <h2>Avatud</h2>
+        <p>T - P kell 9.00 - 16.00</p>
+        <p>E - SULETUD</p>
+      </div>
 
-//     </div>
-//     </div>
-//   );
-// };
+    </div>
+    </div>
+  );
+};
 
-// export default ContactsPage;
+export default ContactsPage;
