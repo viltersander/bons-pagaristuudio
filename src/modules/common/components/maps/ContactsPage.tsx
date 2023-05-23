9cc90488ffdd1c0bf21f604e1f7d6928f1bb2356
viltersander <sander.vilter@ametikool.ee>
2023-05-23 23:53:34 +0300

    initial commit

--- src/modules/common/components/maps/ContactsPage.tsx
+++ src/modules/common/components/maps/ContactsPage.tsx
@@ -1,50 +1,42 @@
-// import React from 'react';
-// import { GoogleMap, Marker, withGoogleMap, withScriptjs } from 'react-google-maps';
+import React from 'react';
+import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
 
-// const MapComponent = withScriptjs(
-//   withGoogleMap(() => (
-//     <GoogleMap
-//       defaultZoom={18}
-//       defaultCenter={{ lat: 58.25256462128469, lng: 22.484175652315106 }}
-//       options={{
-//         disableDefaultUI: true, // Disable default user interface controls
-
-//         styles: [
-//           // Custom map styles can be added here
-//         ],
-//       }}
-//     >
-//       <Marker position={{ lat: 58.25256462128469, lng: 22.484175652315106 }} />
-//     </GoogleMap>
-//   ))
-// );
-
-const ContactsPage: React.FC = () => {
-//   const googleMapsApiKey = ''; // Replace with your actual API key
+const ContactsPage = () => {
+  const googleMapsApiKey = ''; // Replace with your actual API key
+  
+  const mapOptions = {
+    disableDefaultUI: true,
+    styles: [
+      // Custom map styles can be added here
+    ],
+  };
 
   return (
     <div id="contact" style={{ position: 'relative', height: '400px' }}>
-    <div className="p-6">
-      {/* <MapComponent
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}`}
-        loadingElement={<div className="h-full" />}
-        containerElement={<div className="h-full" />}
-        mapElement={<div className="h-full" />}
-      /> */}
-      <div className="absolute top-6 sm:top-10 md:top-10 lg:top-10 left-8 bg-white p-4 shadow-xl border-2 border-black rounded-lg">
-        <h2>Böns Pagaristuudio</h2>
-        <p>Kohtu 1a, Kuressaare, Estonia</p>
-        <p>Tel 5335 8141</p>
-        <p>bonspagar@gmail.com</p>
-      </div>
-      <div className="absolute top-64 sm:top-16 md:top-16 lg:top-16 right-16 sm:right-12 md:right-12 lg:right-12
-       bg-white p-4 shadow-xl border-2 border-black rounded-lg">
-        <h2>Avatud</h2>
-        <p>T - P kell 9.00 - 16.00</p>
-        <p>E - SULETUD</p>
-      </div>
+      <div className="p-6">
+        <LoadScript googleMapsApiKey={googleMapsApiKey}>
+          <GoogleMap
+            mapContainerStyle={{ height: '100%', width: '100%' }}
+            center={{ lat: 58.25256462128469, lng: 22.484175652315106 }}
+            zoom={18}
+            options={mapOptions}
+          >
+            <Marker position={{ lat: 58.25256462128469, lng: 22.484175652315106 }} />
+          </GoogleMap>
+        </LoadScript>
 
-    </div>
+        <div className="absolute top-6 sm:top-10 md:top-10 lg:top-10 left-8 bg-white p-4 shadow-xl border-2 border-black rounded-lg">
+          <h2>Böns Pagaristuudio</h2>
+          <p>Kohtu 1a, Kuressaare, Estonia</p>
+          <p>Tel 5335 8141</p>
+          <p>bonspagar@gmail.com</p>
+        </div>
+        <div className="absolute top-64 sm:top-16 md:top-16 lg:top-16 right-16 sm:right-12 md:right-12 lg:right-12 bg-white p-4 shadow-xl border-2 border-black rounded-lg">
+          <h2>Avatud</h2>
+          <p>T - P kell 9.00 - 16.00</p>
+          <p>E - SULETUD</p>
+        </div>
+      </div>
     </div>
   );
 };
