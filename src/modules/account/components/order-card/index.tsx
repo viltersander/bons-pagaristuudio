ccaa832efcb083fb78164d685cce4fadc0d9b817
viltersander <sander.vilter@ametikool.ee>
2023-06-08 23:23:38 +0300

    formatDate error fix

--- src/modules/account/components/order-card/index.tsx
+++ src/modules/account/components/order-card/index.tsx
@@ -21,7 +21,11 @@ const OrderCard = ({ order }: OrderCardProps) => {
   }, [order]);
 
   const formatDate = (date: string) => {
-    const options = { year: "numeric", month: "long", day: "numeric" };
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
     return new Date(date).toLocaleDateString("et-EE", options);
   };
 
